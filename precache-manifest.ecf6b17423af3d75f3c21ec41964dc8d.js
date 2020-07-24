fe1d3fd70a0566ce42004b29841523f96c1fef14
globalmap <alexander.hanaychuk@gmail.com>
2020-07-24 17:11:07 +0300

    Updates

---
+++ precache-manifest.ecf6b17423af3d75f3c21ec41964dc8d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abz/static/media/menu_icon.6f6bef28.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "9f4635cb2f77050b313f",
+    "url": "/abz/static/js/runtime~main.7392eab7.js"
+  },
+  {
+    "revision": "543d30e852ee37f1e370",
+    "url": "/abz/static/js/main.a059ad04.chunk.js"
+  },
+  {
+    "revision": "1b50ec6e8cb1de5b7ebe",
+    "url": "/abz/static/js/2.15b7ca5d.chunk.js"
+  },
+  {
+    "revision": "543d30e852ee37f1e370",
+    "url": "/abz/static/css/main.f7a8615e.chunk.css"
+  },
+  {
+    "revision": "3261dca1a5b148addbc58a6fafefc7a9",
+    "url": "/abz/index.html"
+  }
+];
